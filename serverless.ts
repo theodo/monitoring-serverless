1c109a78f36e6918d98d48c4e748c337ba522e63
Guillaume Lagrange <guillaume@glagrange.eu>
2021-11-05 16:23:05 +0100

    Add dotenv plugin and use env var for deploy profile

--- serverless.ts
+++ serverless.ts
@@ -1,36 +1,39 @@
-import type { AWS } from '@serverless/typescript';
+import type { AWS } from "@serverless/typescript";
 
-import hello from '@functions/hello';
+import hello from "@functions/hello";
 
 const serverlessConfiguration: AWS = {
-  service: 'monitoring',
-  frameworkVersion: '2',
+  service: "monitoring-backend",
+  frameworkVersion: "2",
   custom: {
     esbuild: {
       bundle: true,
       minify: false,
       sourcemap: true,
-      exclude: ['aws-sdk'],
-      target: 'node14',
-      define: { 'require.resolve': undefined },
-      platform: 'node',
+      exclude: ["aws-sdk"],
+      target: "node14",
+      define: { "require.resolve": undefined },
+      platform: "node",
     },
   },
-  plugins: ['serverless-esbuild'],
+  plugins: ["serverless-esbuild", "serverless-dotenv-plugin"],
   provider: {
-    name: 'aws',
-    runtime: 'nodejs14.x',
+    name: "aws",
+    runtime: "nodejs14.x",
+    region: "eu-west-1",
+    stage: "dev",
+    profile: "${env:AWS_PROFILE}",
+
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
     environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
     },
-    lambdaHashingVersion: '20201221',
+    lambdaHashingVersion: "20201221",
   },
-  // import the function via paths
   functions: { hello },
 };
 
