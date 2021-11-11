a2afb13cd3121c45dee762e6cd8822c7a1500d44
Charles Géry <charles.gery@gmail.com>
2021-11-11 18:54:35 +0100

    feat: instrument lambda with sentry

--- src/functions/generate4XX/handler.ts
+++ src/functions/generate4XX/handler.ts
@@ -1,12 +1,22 @@
-import { middyfy } from '@libs/lambda';
 
+import { BadRequest } from "http-errors";
+const Sentry = require("@sentry/serverless");
+
+
+Sentry.AWSLambda.init({
+  dsn: "https://9d2f4361305e4a01a8242d17053b9b36@o1061187.ingest.sentry.io/6051415",
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 const generate4XX = async () =>  {
   console.error('Error log in 4xx error');
-  return ({
-    statusCode: 400,
-    body: JSON.stringify({message:"Oops, you've made a mistake"}),
-  });
+
+  throw new BadRequest('Ooops you have made a mistake');
+
+  
 }
 
-export const main = middyfy(generate4XX);
+export const main = Sentry.AWSLambda.wrapHandler(generate4XX);
